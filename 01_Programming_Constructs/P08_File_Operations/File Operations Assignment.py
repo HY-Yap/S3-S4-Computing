c81602ba362a5fcd0fd299612f6f131e6c1053b3
Yap Han Yang <yaphanyang09@gmail.com>
2024-08-10 10:02:59 +0800

    yippee

--- 01_Programming_Constructs/P08_File_Operations/File Operations Assignment.py
+++ 01_Programming_Constructs/P08_File_Operations/File Operations Assignment.py
@@ -23,68 +23,115 @@ This function has already been implemented at the back end, you do not need to r
 
 ---------------------------------------------
 '''
-
-
 import os
+
+def bubble_sort(lst):
+    for i in range(len(lst)-1):
+        swapped = False
+        for j in range(len(lst)-i-1):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+                swapped = True
+        if not swapped:
+            break
+    return lst
+
+def insertion_sort(lst):
+    for i in range(1, len(lst)):
+        curr = lst[i]
+        j = i - 1
+        while j >= 0 and curr < lst[j]:
+            j -= 1
+        lst.pop(i)
+        lst.insert(j+1, curr)
+    return lst
+
+def selection_sort(lst):
+    for i in range(len(lst)-1):
+        min_idx = i
+        for j in range(i+1, len(lst)):
+            if lst[j] < lst[min_idx]:
+                min_idx = j
+        lst[i], lst[min_idx] = lst[min_idx], lst[i]
+    return lst
+
+def merge(lst_a, lst_b):
+    lst_c = []
+    while len(lst_a) > 0 and len(lst_b) > 0:
+        lst_c.append(lst_a.pop(0) if lst_a[0] < lst_b[0] else lst_b.pop(0))
+    return lst_c + lst_a + lst_b
+
+def merge_sort(lst):
+    if len(lst) == 1:
+        return lst
+    else:
+        mid = len(lst) // 2
+        left = lst[:mid]
+        right = lst[mid:]
+
+        sorted_left = merge_sort(left)
+        sorted_right = merge_sort(right)
+
+        return merge(sorted_left, sorted_right)
+
+def quick_sort(lst):
+    if len(lst) <= 1:
+        return lst
+    else:
+        pivot = lst[0]
+        left, right = [], []
+
+        for item in lst[1:]:
+            if item <= pivot:
+                left.append(item)
+            else:
+                right.append(item)
+        
+        sorted_left = quick_sort(left)
+        sorted_right = quick_sort(right)
+
+        return sorted_left + [pivot] + sorted_right
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# transfer data into dictionary
-data_file_name = os.path.join(
-    basedir, "assignment_data_files/country_animals.txt")
-with open(data_file_name, "r") as f:
-    raw_data = {}
-    line_counter = 0
-    for line in f:
-        if line_counter % 2 == 0: # to read every alternate line
-            country = line[line.find('<')+1:line.find('>')]
-        else:
-            while ';' in line: # while line is not empty
-                animal = line[0:line.find(';')]
+file = os.path.join(basedir, "assignment_data_files/country_animals.txt")
+with open(file, "r") as f:
+    line = f.readline()
+    animal_country_dict = {}
+    while line:
+        # get country name
+        country = line[line.find('<')+1:line.find('>')]
+        # print(country)
 
-                # update raw_data
-                if animal in raw_data:
-                    if country not in raw_data[animal]:
-                        raw_data[animal].append(country)
-                else:
-                    raw_data[animal] = [country]
+        # get animal names
+        line = f.readline()
+        animal_lst = line.strip(";\n").split(";")
+        # print(animal_lst)
 
-                line = line[line.find(';')+1:] # slice off animal
-        line_counter += 1
+        # add animal names to dict as key
+        # add country names as items (set)
+        for animal in animal_lst:
+            if animal in animal_country_dict:
+                animal_country_dict[animal].add(country)
+            else:
+                animal_country_dict[animal] = set([country])
 
-# sort dictionary keys alphabetically
-my_keys = list(raw_data.keys())
-my_keys.sort()
-sorted_data = {i: raw_data[i] for i in my_keys}
+        # read next line
+        line = f.readline()
 
-# sort values alphabetically
-for animal in sorted_data:
-    sorted_data[animal].sort()
+# print(animal_country_dict)
 
-# write data to new txt file
-data_file_name = os.path.join(
-    basedir, "assignment_data_files/results.txt")
-with open(data_file_name, "w") as f:
-    for animal in sorted_data:
-        f.write(f"<{animal}>:{len(sorted_data[animal])}\n") # animal name
-        f.write(";".join(sorted_data[animal]) + ';\n') # countries
+file = os.path.join(basedir, "assignment_data_files/results.txt")
+with open(file, "w") as f:
+    # sort animals alphabetically
+    animal_lst = quick_sort(list(animal_country_dict.keys()))
+    # print(animal_lst)
 
-# for more concise: use sets to prevent duplicate while adding country
-# also can sort dictionary better
-
-def is_same(file1, file2):
-    file1_path = os.path.join(
-        basedir, file1)
-    file2_path = os.path.join(
-        basedir, file2)
-    
-    with open(file1_path, 'r') as f1, open(file2_path, 'r') as f2:
-        for line1, line2 in zip(f1, f2):
-            if line1 != line2:
-                return False
-    return True
-
-if is_same('assignment_data_files/results.txt', 'assignment_data_files/expected_results.txt'):
-    print("The files are identical!")
-else:
-    print("The files are different :((")
+    # add sorted animals one by one
+    for animal in animal_lst:
+        f.write("<{}>:{}\n".format(animal, len(animal_country_dict[animal])))
+        # country set -> sorted list
+        country_lst = bubble_sort(list(animal_country_dict[animal]))
+        # add sorted countries
+        f.write(";".join(country_lst) + ";\n")
